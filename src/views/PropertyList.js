4ad59347abf905eb9b254bb8b988eb88550187fa
clurts <clu@rts.dk>
2021-11-03 10:07:44 +0100

    add fetch and property card

--- src/views/PropertyList.js
+++ src/views/PropertyList.js
@@ -1,13 +1,33 @@
+import { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
+import axios from "axios";
+import PropertyCard from "../components/PropertyCard";
+import TwoColumns from "../components/TwoColumns";
 
 const PropertyList = () => {
-	return (
-		<>
+	const [properties, setProperties] = useState();
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		axios("https://dinmaegler.herokuapp.com/homes").then((response) => {
+			setProperties(response.data);
+			setLoading(false);
+		});
+	}, []);
+	return loading ? (
+		<h3 className="widest centered">loading...</h3>
+	) : (
+		<div className="widest centered">
 			<Helmet>
 				<title>Boliger til salg - Din Mægler</title>
 			</Helmet>
 			<h1>This is property list the view</h1>
-		</>
+			<TwoColumns>
+				{properties.map((property) => (
+					<PropertyCard data={property} />
+				))}
+			</TwoColumns>
+		</div>
 	);
 };
 
