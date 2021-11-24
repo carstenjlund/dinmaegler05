c78563ceb4ab026255036974218683d9bc3a1371
clurts <clu@rts.dk>
2021-11-24 09:14:07 +0100

    update to use react-router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { Link } from "@reach/router";
+import { Link } from "react-router-dom";
 import Navigation from "./Navigation";
 import { theme } from "./theme";
 import { useContext } from "react";
